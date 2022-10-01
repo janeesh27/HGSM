6e7d4dd41db895061aa24eade1bf7d83ca4d54ca
janeesh <janeesh.tin@gmail.com>
2022-10-01 22:42:32 +0530

    logo added

--- componets/Navbar.jsx
+++ componets/Navbar.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import Image from 'next/image';
 
 const Navbar = () => {
   return (
-   <div className='dd'></div>
-  )
-}
+    <div className='fixed w-full h-20 shadow-xl z-[100]'>
+      <div className='flex justify-between items-center h-full w-full px-2 2xl:px-16'>
+      <Image src='/../public/media/Logo.gif' alt='/' width='140' height='70' />
 
-export default Navbar
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
