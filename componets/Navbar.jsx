e993dd2bdad95b78a15d43864f6ee1744e87fb51
janeesh <janeesh.tin@gmail.com>
2022-10-02 04:19:19 +0530

    link working fixed

--- componets/Navbar.jsx
+++ componets/Navbar.jsx
@@ -1,12 +1,25 @@
 import React from "react";
-import Image from 'next/image';
+import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <div className='fixed w-full h-20 shadow-xl z-[100]'>
-      <div className='flex justify-between items-center h-full w-full px-2 2xl:px-16'>
-      <Image src='/../public/media/Logo.gif' alt='/' width='140' height='70' />
-
+    <div className="fixed w-full h-20 shadow-xl z-[100]">
+      <div className="flex justify-between items-center h-full w-full px-2 2xl:px-16">
+        <h2>Janeesh Pal Singh</h2>
+      </div>
+      <div>
+        <ul>
+          <Link href="/about">
+            <li className="ml-10 text-sm hover:border-b">About</li>
+          </Link>
+          <Link href="/skills">
+            <li className="ml-10 text-sm hover:border-b">skills</li>
+          </Link>
+          <Link href="/projects">
+            <li className="ml-10 text-sm hover:border-b">projects</li>
+          </Link>
+        </ul>
       </div>
     </div>
   );
