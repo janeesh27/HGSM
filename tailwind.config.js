edfd0d6da99849f38e06030ae94fd762b3f92fe7
janeesh <janeesh.tin@gmail.com>
2022-10-01 19:22:42 +0530

    custom styling added to globals.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
-/** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",],
   theme: {
     extend: {},
   },
