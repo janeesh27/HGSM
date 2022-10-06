00afaa134f8b896ac076bdef732c31e76fc883fb
janeesh <janeesh.tin@gmail.com>
2022-10-07 03:33:10 +0530

    no progress

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,20 +7,20 @@ const Navbar = () => {
     <div className="fixed w-full h-20 shadow-xl z-[100]">
       <div className="flex justify-between shadow-text-3px items-center h-full w-full px-2 2xl:px-16">
         <h2>Mandolia Institute</h2>
+        <div className="flex-initial">
+          <ul>
+            <Link href="/aboutus">
+              <li className="ml-10 text-3xl hover:border-b">About us</li>
+            </Link>
+            <Link href="/gallery">
+              <li className="ml-10 text-3xl hover:border-b">gallery</li>
+            </Link>
+            <Link href="/contactus">
+              <li className="ml-10 text-3xl hover:border-b">contact us</li>
+            </Link>
+          </ul>
+        </div>
       </div>
-    
-        <ul>
-          <Link href="/about">
-            <li className="ml-10 text-red-900 text-sm hover:border-b">About</li>
-          </Link>
-          <Link href="/skills">
-            <li className="ml-10 text-sm hover:border-b">skills</li>
-          </Link>
-          <Link href="/projects">
-            <li className="ml-10 text-sm hover:border-b">projects</li>
-          </Link>
-        </ul>
-      
     </div>
   );
 };
