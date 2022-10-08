66341df1daa2cfa6f2ee2efbad2383d967bc609a
janeesh <janeesh.tin@gmail.com>
2022-10-08 18:26:22 +0530

    nav bar designed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,11 +4,12 @@ import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <div className="fixed w-full h-20 shadow-xl z-[100]">
-      <div className="flex justify-between shadow-text-3px items-center h-full w-full px-2 2xl:px-16">
-        <h2>Mandolia Institute</h2>
-        <div className="flex-initial">
-          <ul>
+    <div className="fixed bg-white w-full h-20 shadow-xl z-[100]">
+      <div className="flex justify-between items-center h-full w-full px-2 2xl:px-16">
+        <Image src='/../public/media/Logo1.png' alt="/" width='120' height='60' />
+        <h2>MANDOLIA INSTITUTE</h2>
+        <div>
+        <ul>
             <Link href="/aboutus">
               <li className="ml-10 text-3xl hover:border-b">About us</li>
             </Link>
