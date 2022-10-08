dea9d756e255376461db9588ee8bb51439892532
janeesh <janeesh.tin@gmail.com>
2022-10-08 20:07:12 +0530

    updated logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,18 +6,19 @@ const Navbar = () => {
   return (
     <div className="fixed bg-white w-full h-20 shadow-xl z-[100]">
       <div className="flex justify-between items-center h-full w-full px-2 2xl:px-16">
-        <Image src='/../public/media/Logo1.png' alt="/" width='120' height='60' />
+       <span> <Image src='/../public/media/Logo2.png' alt="/" width='100' height='70' />
+        </span>
         <h2>MANDOLIA INSTITUTE</h2>
         <div>
-        <ul>
+        <ul className="">
             <Link href="/aboutus">
-              <li className="ml-10 text-3xl hover:border-b">About us</li>
+              <li className="ml-10 text-2xl hover:border-b">About us</li>
             </Link>
             <Link href="/gallery">
-              <li className="ml-10 text-3xl hover:border-b">gallery</li>
+              <li className="ml-10 text-2xl hover:border-b">gallery</li>
             </Link>
             <Link href="/contactus">
-              <li className="ml-10 text-3xl hover:border-b">contact us</li>
+              <li className="ml-10 text-2xl hover:border-b">contact us</li>
             </Link>
           </ul>
         </div>
