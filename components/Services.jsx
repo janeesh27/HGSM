0bbd7f6025e4244665f9f5782dece81b49675749
janeesh <janeesh.tin@gmail.com>
2022-10-11 01:41:52 +0530

    formating done complete(two columns to be filled) and center alingnment

--- components/Services.jsx
+++ components/Services.jsx
@@ -4,14 +4,14 @@ const Services = () => {
   return (
     <div className="w-full lg:h-screen p-2">
       <div className="max-w-[1240px] mx-auto flex flex-col text-center h-full">
-        <h1 className="uppercase">we offer</h1>
+        <h1 className="text-yellow-500 mb-20">We Offer</h1>
         <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
        
        
-        <div className="p-6 shadow-xl rounded-md hover:scale-105 ease-in duration-300">
+        <div className="p-6 pl-6 shadow-xl rounded-md hover:scale-105 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
-                <Image
+                <Image 
                   src="/../public/media/cbse.png"
                   alt="/"
                   width="100px"
@@ -29,13 +29,13 @@ const Services = () => {
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <Image
-                  src="/../public/media/cbse.png"
+                  src="/../public/media/jeemains.png"
                   alt="/"
                   width="100px"
                   height="100px"
                 />
                 <div className="flex flex-col items-center justify-center">
-                  <h3>Boards Preprations</h3>
+                  <h3>JEE Mains</h3>
                 </div>
               </div>
             </div>
@@ -47,13 +47,13 @@ const Services = () => {
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <Image
-                  src="/../public/media/cbse.png"
+                  src="/../public/media/neet.jpg"
                   alt="/"
                   width="100px"
                   height="100px"
                 />
                 <div className="flex flex-col items-center justify-center">
-                  <h3>Boards Preprations</h3>
+                  <h3>NEET UG</h3>
                 </div>
               </div>
             </div>
@@ -64,13 +64,13 @@ const Services = () => {
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <Image
-                  src="/../public/media/cbse.png"
+                  src="/../public/media/overall.jpg"
                   alt="/"
                   width="100px"
                   height="100px"
                 />
                 <div className="flex flex-col items-center justify-center">
-                  <h3>Boards Preprations</h3>
+                  <h3>Overall Devlopment</h3>
                 </div>
               </div>
             </div>
@@ -82,13 +82,13 @@ const Services = () => {
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <Image
-                  src="/../public/media/cbse.png"
+                  src="/../public/media/testseries.jpg"
                   alt="/"
                   width="100px"
                   height="100px"
                 />
                 <div className="flex flex-col items-center justify-center">
-                  <h3>Boards Preprations</h3>
+                  <h3>Regular Tests</h3>
                 </div>
               </div>
             </div>
@@ -99,13 +99,13 @@ const Services = () => {
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <Image
-                  src="/../public/media/cbse.png"
+                  src="/../public/media/notes.jpg"
                   alt="/"
                   width="100px"
                   height="100px"
                 />
                 <div className="flex flex-col items-center justify-center">
-                  <h3>Boards Preprations</h3>
+                  <h3>Personalized Notes</h3>
                 </div>
               </div>
             </div>
