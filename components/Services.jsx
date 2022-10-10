d731d142aae71b946a61144a2ad65e6e589e0c15
janeesh <janeesh.tin@gmail.com>
2022-10-11 01:12:41 +0530

    service section created

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,15 +1,150 @@
 import React from "react";
-
+import Image from "next/image";
 const Services = () => {
   return (
     <div className="w-full lg:h-screen p-2">
       <div className="max-w-[1240px] mx-auto flex flex-col text-center h-full">
         <h1 className="uppercase">we offer</h1>
         <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
-        
-        <div>
-
+       
+       
+        <div className="p-6 shadow-xl rounded-md hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/media/cbse.png"
+                  alt="/"
+                  width="100px"
+                  height="100px"
+                />
+                <div className="flex flex-col items-center justify-center">
+                  <h3>Boards Preprations</h3>
+                </div>
+              </div>
+            </div>
         </div>
+        
+         
+        <div className="p-6 shadow-xl rounded-md hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/media/cbse.png"
+                  alt="/"
+                  width="100px"
+                  height="100px"
+                />
+                <div className="flex flex-col items-center justify-center">
+                  <h3>Boards Preprations</h3>
+                </div>
+              </div>
+            </div>
+        </div>
+        
+
+ 
+        <div className="p-6 shadow-xl rounded-md hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/media/cbse.png"
+                  alt="/"
+                  width="100px"
+                  height="100px"
+                />
+                <div className="flex flex-col items-center justify-center">
+                  <h3>Boards Preprations</h3>
+                </div>
+              </div>
+            </div>
+        </div>
+        
+ 
+        <div className="p-6 shadow-xl rounded-md hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/media/cbse.png"
+                  alt="/"
+                  width="100px"
+                  height="100px"
+                />
+                <div className="flex flex-col items-center justify-center">
+                  <h3>Boards Preprations</h3>
+                </div>
+              </div>
+            </div>
+        </div>
+        
+
+ 
+        <div className="p-6 shadow-xl rounded-md hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/media/cbse.png"
+                  alt="/"
+                  width="100px"
+                  height="100px"
+                />
+                <div className="flex flex-col items-center justify-center">
+                  <h3>Boards Preprations</h3>
+                </div>
+              </div>
+            </div>
+        </div>
+        
+         
+        <div className="p-6 shadow-xl rounded-md hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/media/cbse.png"
+                  alt="/"
+                  width="100px"
+                  height="100px"
+                />
+                <div className="flex flex-col items-center justify-center">
+                  <h3>Boards Preprations</h3>
+                </div>
+              </div>
+            </div>
+        </div>
+        
+         
+        <div className="p-6 shadow-xl rounded-md hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/media/cbse.png"
+                  alt="/"
+                  width="100px"
+                  height="100px"
+                />
+                <div className="flex flex-col items-center justify-center">
+                  <h3>Boards Preprations</h3>
+                </div>
+              </div>
+            </div>
+        </div>
+        
+         
+        <div className="p-6 shadow-xl rounded-md hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/media/cbse.png"
+                  alt="/"
+                  width="100px"
+                  height="100px"
+                />
+                <div className="flex flex-col items-center justify-center">
+                  <h3>Boards Preprations</h3>
+                </div>
+              </div>
+            </div>
+        </div>
+        
 
         </div>
       </div>
