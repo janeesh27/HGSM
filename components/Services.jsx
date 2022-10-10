d92d4751207be32b37512b7c39574df2798e7f18
janeesh <janeesh.tin@gmail.com>
2022-10-11 00:10:12 +0530

    media added

--- components/Services.jsx
+++ components/Services.jsx
@@ -6,6 +6,10 @@ const Services = () => {
       <div className="max-w-[1240px] mx-auto flex flex-col text-center h-full">
         <h1 className="uppercase">we offer</h1>
         <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+        
+        <div>
+
+        </div>
 
         </div>
       </div>
