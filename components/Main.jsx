95a3e8607236c0738e68f0dbe882973736a352b6
janeesh <janeesh.tin@gmail.com>
2022-10-10 15:38:38 +0530

    main component problem jugad

--- components/Main.jsx
+++ components/Main.jsx
@@ -4,16 +4,13 @@ import Image from "next/image";
 const Main = () => {
   return (
     <div className='w-full h-screen text-center'>
-      <div className="max-w-[1240px] w-full h-full p-2 mx-auto flex items-center">
-            <div className="">
-            
-<Image className="opacity-50"
-src='/../public/media/bg.jpg'
-alt="/"
-height='500'
-width='900'
- />
+      <div className="max-w-[1240px] w-full h-full p-2 mx-auto flex justify-center items-center">
+         <div>
+        <p className="uppercase text-sm tracking-widest font-semibold">
+        serving excellence since 2002..
+         </p>
             </div>
+
          </div>       
     </div>
   );
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
 
   return (
     <div className="fixed bg-white w-full h-20 shadow-xl z-[100]">
-      <div className="flex justify-between items-center h-full w-full px-2 2xl:px-16">
+      <div className="flex justify-between items-center order-1 h-full w-full px-2 2xl:px-16">
         <Image
           src="/../public/media/Logo2.png"
           alt="/"
