b175d5db7401171b445808648804cf073304d5c6
janeesh <janeesh.tin@gmail.com>
2022-10-21 01:00:15 +0530

    form templete added

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const form = () => {
   return (
-    <div>form</div>
-  )
-}
+    <div className=" bg-[#ffff3d] ml-5 pl-4 min-h-full w-80">
+      <div className="flex justify-start items-start  ">
+        <h1 className="uppercase text-4xl text-black">contact us</h1>
+      </div>
+      <div>
+        <form>
+          <div className="my-4 mx-auto"><p>Name</p>
+            <input type='text' />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default form
+export default form;
--- components/gallery/Gallery.jsx
+++ components/gallery/Gallery.jsx
@@ -3,6 +3,11 @@ import Image from 'next/image'
 
 const Gallery = () => {
   return (
+    <>
+    <div className='text-center tracking-wider m-8 uppercase'>
+    <h1>Gallery</h1>
+
+    </div>
     <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
       <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
         {/* Images will go here */}
@@ -18,17 +23,20 @@ const Gallery = () => {
         <Image src='/../public/gallery/4.jpg'
         width='500'
         height='500' />
-        <Image src='/../public/gallery/5.jpg'
+        <Image src='/../public/gallery/4.jpg'
         width='500'
         height='500' />
-        <Image src='/../public/gallery/6.jpg'
+        <Image src='/../public/gallery/4.jpg'
+        width='500'
+        height='500' />
+         <Image src='/../public/gallery/1.jpg'
         width='500'
         height='500' />
          <Image src='/../public/gallery/1.jpg'
         width='500'
         height='500' />
       </div>
-    </div>
+    </div></>
   )
 }
 
