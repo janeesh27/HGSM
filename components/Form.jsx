3f7b8d4b126386bd48583c1f48ec3aceb2e3c0cc
janeesh <janeesh.tin@gmail.com>
2022-10-21 16:02:21 +0530

    basic design fixed for from

--- components/Form.jsx
+++ components/Form.jsx
@@ -2,17 +2,26 @@ import React from "react";
 
 const form = () => {
   return (
-    <div className=" bg-[#ffff3d] ml-5 pl-4 min-h-full w-80">
+  <div className="bg-[grey]">
+    <div className=" bg-gradient-to-b from-yellow-600 to-yellow-300 p-5 min-h-full w-80">
       <div className="flex justify-start items-start  ">
         <h1 className="uppercase text-4xl text-black">contact us</h1>
       </div>
       <div>
         <form>
-          <div className="my-4 mx-auto"><p>Name</p>
-            <input type='text' />
+          <div className="my-4 mx-auto">
+            <p className="text-2xl">Name</p>
+            <input type="text" className="w-60 my-2" />
+            <p className="text-2xl">Class</p>
+            <input type="text" className="w-60 my-2" />
+            <p className="text-2xl">Mobile</p>
+            <input type="text" className="w-60 my-2" />
+            <p className="text-2xl">Email</p>
+            <input type="text" className="w-60 my-2" />
           </div>
         </form>
       </div>
+    </div>
     </div>
   );
 };
