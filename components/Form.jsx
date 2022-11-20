671e4f9848b89cca570fa9e283d219853d91898d
janeesh <janeesh.tin@gmail.com>
2022-11-21 01:41:12 +0530

    content and functionality left for form

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,60 +1,135 @@
 import { Input } from "postcss";
 import React from "react";
+import Link from "next/link";
+import { AiFillFacebook, AiFillInstagram } from "react-icons/ai";
+import { HiOutlineChevronDoubleUp } from "react-icons/hi";
 
 const form = () => {
   return (
     <div id="form" className="w-full lg:h-screen">
-      {/* // right */}
+      <div className="max-w-[1240px] m-auto px-2 py-16 w-full ">
+        <h2 className="py-4">Get In Touch</h2>
+        <div className="grid lg:grid-cols-5 gap-8">
+          {/* left */}
+          <div className="col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4">
+            <div className="lg:p-4 h-full ">
+              <div>
+                <h1>c</h1>
+              </div>
+              <div>
+                <h2 className="py-2">Clint Briley</h2>
+                <p>Front-End Developer</p>
+                <p className="py-4">
+                  I am available for freelance or full-time positions. Contact
+                  me and let&apos;s talk.
+                </p>
+              </div>
+              <div>
+                <p className="uppercase pt-8">Connect With Us</p>
+                <div className="flex items-center justify-start gap-3 py-4">
+                  <a
+                    href="https://www.linkedin.com/in/clint-briley-50056920a/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <div className="rounded-full shadow-lg shadow-gray-400 p-2 cursor-pointer hover:scale-110 ease-in duration-300">
+                      {/* <FaLinkedinIn /> */}
+                      <AiFillFacebook size={35} />
+                    </div>
+                  </a>
+                  <a
+                    href="https://github.com/fireclint"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <div className="rounded-full shadow-lg shadow-gray-400 p-2 cursor-pointer hover:scale-110 ease-in duration-300">
+                      {/* <FaGithub /> */}
+                      <AiFillInstagram size={35} />
+                    </div>
+                  </a>
 
-      <div className="col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4"></div>
-      <div className="p-4">
-        <form>
-          <div className="grid md:grid-cols-2 gap-4 w-full py-2">
-            <div className="flex flex-col">
-              <label className="uppercase text-sm py-2">Name</label>
-              <input
-                className="border-2 rounded-lg p-3 flex border-gray-300"
-                type="text"
-                name="name"
-              />
-            </div>
-            <div className="flex flex-col">
-              <label className="uppercase text-sm py-2">Phone Number</label>
-              <input
-                className="border-2 rounded-lg p-3 flex border-gray-300"
-                type="text"
-                name="phone"
-              />
+                  {/* <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                  
+                </div>
+                <Link href='/resume'>
+                  <a>
+                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                      
+                    </div>
+                  </a>
+                </Link> */}
+                </div>
+              </div>
             </div>
           </div>
-          <div className="flex flex-col py-2">
-            <label className="uppercase text-sm py-2">Email</label>
-            <input
-              className="border-2 rounded-lg p-3 flex border-gray-300"
-              type="email"
-              name="email"
-            />
+
+          {/* right */}
+          <div className="col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4">
+            <div className="p-4">
+              <form>
+                <div className="grid md:grid-cols-2 gap-4 w-full py-2">
+                  <div className="flex flex-col">
+                    <label className="uppercase text-sm py-2">Name</label>
+                    <input
+                      className="border-2 rounded-lg p-3 flex border-gray-300"
+                      type="text"
+                      name="name"
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <label className="uppercase text-sm py-2">
+                      Phone Number
+                    </label>
+                    <input
+                      className="border-2 rounded-lg p-3 flex border-gray-300"
+                      type="text"
+                      name="phone"
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-col py-2">
+                  <label className="uppercase text-sm py-2">Email</label>
+                  <input
+                    className="border-2 rounded-lg p-3 flex border-gray-300"
+                    type="email"
+                    name="email"
+                  />
+                </div>
+                <div className="flex flex-col py-2">
+                  <label className="uppercase text-sm py-2">Subject</label>
+                  <input
+                    className="border-2 rounded-lg p-3 flex border-gray-300"
+                    type="text"
+                    name="subject"
+                  />
+                </div>
+                <div className="flex flex-col py-2">
+                  <label className="uppercase text-sm py-2">Message</label>
+                  <textarea
+                    className="border-2 rounded-lg p-3 border-gray-300"
+                    rows="10"
+                    name="message"
+                  ></textarea>
+                </div>
+                <button className="w-full p-4 text-[black] font-semibold bg-gradient-to-r from-yellow-600 to-yellow-300 mt-4">
+                  Send Message
+                </button>
+              </form>
+            </div>
           </div>
-          <div className="flex flex-col py-2">
-            <label className="uppercase text-sm py-2">Subject</label>
-            <input
-              className="border-2 rounded-lg p-3 flex border-gray-300"
-              type="text"
-              name="subject"
-            />
-          </div>
-          <div className="flex flex-col py-2">
-            <label className="uppercase text-sm py-2">Message</label>
-            <textarea
-              className="border-2 rounded-lg p-3 border-gray-300"
-              rows="10"
-              name="message"
-            ></textarea>
-          </div>
-          <button className="w-full p-4 text-[black] font-semibold bg-gradient-to-r from-yellow-600 to-yellow-300 mt-4">
-            Send Message
-          </button>
-        </form>
+        </div>
+        <div className="flex justify-center py-12">
+          <Link href="/">
+            <a>
+              <div className="rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
+                <HiOutlineChevronDoubleUp
+                  className="text-[gold]"
+                  size={50}
+                />
+              </div>
+            </a>
+          </Link>
+        </div>
       </div>
     </div>
   );
