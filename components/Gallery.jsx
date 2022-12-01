62e3468b86ea08a1f9d6afda1c068871905c7873
janeesh <janeesh.tin@gmail.com>
2022-12-01 18:43:34 +0530

    img problem

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -10,7 +10,7 @@ const Gallery = () => {
       <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
         <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {/* Images will go here */}
-          <Image
+          <img
             src="/../public/gallery/1.jpg"
             alt="main buliding"
             width="500"
