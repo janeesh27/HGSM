9b67997a153f33a12ec656e5f3ac8194f315d26c
janeesh <janeesh.tin@gmail.com>
2022-12-02 19:37:23 +0530

    dummy text

--- components/About.jsx
+++ components/About.jsx
@@ -6,15 +6,20 @@ const About = () => {
       <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
         <div className="col-span-2">
           <p className="text-3xl font-semibold mb-8">About Us</p>
-          <p></p>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
         </div>
         <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-110 ease-in duration-300">
-          <Image
-            src="/media/bg.jpg"
-            alt="Reload"
-            height="500"
-            width="800"
-          />
+          <Image src="/media/bg.jpg" alt="Reload" height="500" width="800" />
         </div>
       </div>
     </div>
