e1b5be41c8932708c401e94e9129c51c0d819ba9
janeesh <janeesh.tin@gmail.com>
2022-12-02 19:50:58 +0530

    '

--- components/About.jsx
+++ components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
           <p className="text-3xl font-semibold mb-8">About Us</p>
           <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
+            industry. Lorem Ipsum has been the industry&aposs standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
             and scrambled it to make a type specimen book. It has survived not
             only five centuries, but also the leap into electronic typesetting,
